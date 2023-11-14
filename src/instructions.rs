ea58c3ada1721b737880a14470f460323a28543b
MiracleSNeko <miracle.neko@qq.com>
2023-11-14 08:47:08 +0000

    dev(kernel): add virtual machine kernel

--- src/instructions.rs
+++ src/instructions.rs
@@ -4,10 +4,13 @@ use super::kernel;
 mod implement {
     use super::kernel::Kernel;
     use anyhow::Result;
+    #[cfg(not(feature = "meta_init"))]
     use enum_dispatch::enum_dispatch;
     use wgse_kernel::types::{common::Argument, wrapper::BinVec};
     #[cfg(feature = "meta_init")]
     use wgse_utils::wgse_command_interface;
+    #[cfg(not(feature = "meta_init"))]
+    use wgse_utils::wgse_command_trait_impl;
 
     #[cfg(feature = "meta_init")]
     pub trait WgseCommand {
@@ -21,6 +24,10 @@ mod implement {
         #[allow(unused_variables)]
         fn execute(&self, kernel: &mut Kernel, args: &BinVec<Argument>) -> Result<()>;
     }
+
+    #[cfg(not(feature = "meta_init"))]
+    #[wgse_command_trait_impl(WgseCommand)]
+    pub enum WgseCommands {}
 }
 
 #[cfg(not(feature = "meta_collect"))]
